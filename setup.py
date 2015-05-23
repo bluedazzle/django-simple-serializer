026bca162560b9b2fc764fd13969e2d53c45c6df
rapospectre <rapospectre@163.com>
2015-05-23 21:15:12 +0800

    add license

---
+++ setup.py
@@ -0,0 +1,53 @@
+import codecs
+import os
+import sys
+ 
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+def read(fname):
+    return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+NAME = "django-simple-serializer"
+ 
+PACKAGES = ["src", ]
+ 
+DESCRIPTION = "Django Simple Serializer is a serializer to help user serialize django data or python list into json\xml\dict data in a simple way."
+ 
+LONG_DESCRIPTION = read("README.rst")
+ 
+KEYWORDS = "django serializer"
+ 
+AUTHOR = "RaPoSpectre"
+ 
+AUTHOR_EMAIL = "rapospectre@gmail.com"
+ 
+URL = ""
+ 
+VERSION = "0.0.1"
+
+LICENSE = "MIT"
+
+setup(
+    name = NAME,
+    version = VERSION,
+    description = DESCRIPTION,
+    long_description = LONG_DESCRIPTION,
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+    keywords = KEYWORDS,
+    author = AUTHOR,
+    author_email = AUTHOR_EMAIL,
+    url = URL,
+    license = LICENSE,
+    packages = PACKAGES,
+    include_package_data=True,
+    zip_safe=True,
+)
